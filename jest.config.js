285834bfde8a4f79f388652cd89dad76b902e9c0
geunyoungno <geunyoungno@gmail.com>
2023-01-30 21:08:59 +0900

    add jest

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+const nextJest = require('next/jest');
+const { parse } = require('jsonc-parser');
+const path = require('path');
+const fs = require('fs');
+const { pathsToModuleNameMapper } = require('ts-jest');
+
+const tsconfig = parse(fs.readFileSync(path.join(__dirname, 'tsconfig.json')).toString());
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './src',
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+
+  // If you're using [Module Path Aliases](https://nextjs.org/docs/advanced-features/module-path-aliases),
+  // you will have to add the moduleNameMapper in order for jest to resolve your absolute paths.
+  // The paths have to be matching with the paths option within the compilerOptions in the tsconfig.json
+  // For example:
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, { prefix: '<rootDir>/src' }),
+
+  testMatch: ['**/__tests__/*.(ts|tsx)', '**/__test__/*.(ts|tsx)'],
+  testPathIgnorePatterns: ['/node_modules/', 'example/', 'dist/'],
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);

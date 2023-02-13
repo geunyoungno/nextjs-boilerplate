28c3ad146a3992b338eabe07f996e242ac7cc121
geunyoungno <geunyoungno@gmail.com>
2023-02-13 21:37:04 +0900

    add Measuring performance

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
-import type { AppProps } from 'next/app';
 import '#styles/globals.css';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  // eslint-disable-next-line no-console
+  console.log(metric);
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

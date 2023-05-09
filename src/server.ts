d421012c0a9e1dc497a86c85734931e3458500bc
geunyoungno <geunyoungno@gmail.com>
2023-05-09 21:38:24 +0900

    add serverInfo when registering fastifyNextjs

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,30 @@
 import fastifyNextjs from '@fastify/nextjs';
-import fastify, { FastifyRequest } from 'fastify';
+import fastify, { FastifyListenOptions, FastifyRequest } from 'fastify';
 
 const server = fastify();
+
+const serverInfo = {
+  host: 'localhost',
+  port: 3000,
+} satisfies FastifyListenOptions;
 
 async function noOpParser(_req: FastifyRequest, payload: unknown) {
   return payload;
 }
 
-server.register(fastifyNextjs).after(() => {
-  server.addContentTypeParser('text/plain', noOpParser);
-  server.addContentTypeParser('application/json', noOpParser);
-  server.next('/*');
-  server.next('/api/*', { method: undefined });
-});
+server
+  .register(fastifyNextjs, {
+    hostname: serverInfo.host,
+    port: serverInfo.port,
+  })
+  .after(() => {
+    server.addContentTypeParser('text/plain', noOpParser);
+    server.addContentTypeParser('application/json', noOpParser);
+    server.next('/*');
+    server.next('/api/*', { method: undefined });
+  });
 
-server.listen({ port: 3000, host: '0.0.0.0' }, (err) => {
+server.listen(serverInfo, (err) => {
   if (err) throw err;
 
   // eslint-disable-next-line no-console
